40fbf4773b106c66e5e3142fc3227fc0fb655433
AliHaine <alitr57120@gmail.com>
2024-03-13 15:22:02 +0100

    update CGI

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -2,8 +2,10 @@
 
 #include "incs.hpp"
 #include "Request.hpp"
+#include "Cgi.hpp"
 
 class Client;
+class Cgi;
 
 class Response {
 	public:
@@ -12,12 +14,14 @@ class Response {
 
 		std::string &getHeader();
 		std::string &getContent();
+        void setContent(const std::string &content);
         std::string &getUri();
         bool getGenerated();
         void handleRequestError(Server* server, const std::string & uri);
         bool handleReturn(Server *server);
         bool findStatusCode(std::map<unsigned int, std::string>::iterator itf, std::map<unsigned int, std::string>& error_code);
         bool findReturnLocations(Server* server);
+        bool isCgi(const std::string &file_type);
 
         std::ifstream *_contentFile; //For local files
         long long _contentSize;
@@ -25,10 +29,6 @@ class Response {
         void headerGenBuilder(std::string file_type);
         void headerFileBuilder(std::string file_type);
         void generateAutoindex(Request & req);
-        void cgiBuilder(const Request &request);
-		bool isCgi(const std::string &file_type);
-        void pipeCreatorAndExec(char **argv);
-        void closeAllPipe();
         std::string getCodeHeader(std::string * path, Server* server, const std::string & uri);
 
         Server *server; // Attached server
@@ -36,9 +36,4 @@ class Response {
 		std::string _header;
         std::string _content; // For auto-generated content like autoindex
         bool _isGenerated;
-
-        char **_env;
-        int _pid;
-        int _pipe_in[2]; //Used by server to send data to cgi
-        int _pipe_out[2]; // Used by cgi to send data to server
 };
--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -20,7 +20,9 @@ Response::Response(Server & server, Request &request) : _contentFile(0), server(
             return;
     }
 	if (isCgi(request.getFileType())) {
-        cgiBuilder(request);
+        Cgi(*this, request);
+        headerFileBuilder("text/html");
+        return;
 	}
 
     this->_contentFile = new std::ifstream;
@@ -42,6 +44,13 @@ bool Response::findStatusCode(std::map<unsigned int, std::string>::iterator itf,
 {
     std::map<unsigned int, std::string>::iterator it = __defaultErrorCodes.errorCodes.find(itf->first);
     return it != error_code.end();
+}
+
+bool Response::isCgi(const std::string &file_type) {
+    //temp system
+    if (file_type.find("py") != std::string::npos)
+        return true;
+    return false;
 }
 
 bool Response::findReturnLocations(Server* server)
@@ -134,122 +143,6 @@ void Response::generateAutoindex(Request & req) {
     this->_isGenerated= true;
     if (DEBUG)
         std::cout << "Autoindex Generated" << std::endl;
-}
-
-//env example
-/*AUTH_TYPE=Basic
-CONTENT_LENGTH=
-CONTENT_TYPE=
-DOCUMENT_ROOT=./
-GATEWAY_INTERFACE=CGI/1.1
-HTTP_COOKIE=
-PATH_INFO=
-PATH_TRANSLATED=.//
-QUERY_STRING=
-REDIRECT_STATUS=200
-REMOTE_ADDR=localhost:8002
-REQUEST_METHOD=GET
-REQUEST_URI=/cgi-bin/acc.py
-        SCRIPT_FILENAME=acc.py
-SCRIPT_NAME=cgi-bin/acc.py
-SERVER_NAME=localhost
-SERVER_PORT=8002
-SERVER_PROTOCOL=HTTP/1.1
-SERVER_SOFTWARE=AMANIX*/
-
-void Response::cgiBuilder(const Request &request) {
-    std::string runner = "/usr/bin/python3";
-    std::string script = "experiment/expe_ali/site/testpy.py";
-    std::string scriptname = "testpy.py";
-
-    //argv builder
-    char **argv = new char*[3];
-    for (int i = 0; i < 3; i++)
-        argv[i] = new char[50];
-    strcpy(argv[0], runner.c_str());  // Path to runner
-    strcpy(argv[1], script.c_str());  // Path to script
-    argv[2] = 0;
-
-    //env builder
-    std::map<std::string, std::string> env_as_map;
-    env_as_map["AUTH_TYPE="] = "Basic";
-    env_as_map["CONTENT_LENGTH="] = "";
-    env_as_map["CONTENT_TYPE="] = "";       //request.getFileType();
-    env_as_map["GATEWAY_INTERFACE="] = "CGI/1.1";
-    env_as_map["SCRIPT_NAME="] = script.c_str();
-    env_as_map["SCRIPT_FILENAME="] = scriptname.c_str();
-    //this->_env["PATH_TRANSLATED"] = it_loc->getRootLocation() + (this->_env["PATH_INFO"] == "" ? "/" : this->_env["PATH_INFO"]);
-    //this->_env["QUERY_STRING"] = decode(req.getQuery());
-    //this->_env["REMOTE_ADDR"] = req.getHeader("host");
-    env_as_map["SERVER_NAME="] = "localhost";
-    env_as_map["SERVER_PORT="] = "8080";
-    env_as_map["REQUEST_METHOD="] = request.getMethod();
-    //this->_env["HTTP_COOKIE"] = req.getHeader("cookie");
-    //this->_env["DOCUMENT_ROOT"] = it_loc->getRootLocation();
-    //this->_env["REQUEST_URI"] = req.getPath() + req.getQuery();*
-    env_as_map["SERVER_PROTOCOL="] = "HTTP/1.1";
-    env_as_map["REDIRECT_STATUS="] = "200";
-    env_as_map["SERVER_SOFTWARE="] = "AMANIX";
-
-    this->_env = new char*[env_as_map.size() + 1];
-    std::map<std::string, std::string>::const_iterator it = env_as_map.begin();
-    for (int i = 0; it != env_as_map.end(); it++, i++) {
-        std::string key_and_value = it->first + it->second;
-        this->_env[i] = strdup(key_and_value.c_str());
-    }
-    this->_env[env_as_map.size()] = 0;
-
-    pipeCreatorAndExec(argv);
-    //chdir("experiment/expe_ali/site/");
-
-    //readfrom pipe
-    char t[1024];
-    read(_pipe_out[0], t, 1024);
-    std::cout << "res= " << t << std::endl;
-    closeAllPipe();
-}
-
-bool Response::isCgi(const std::string &file_type) {
-	//temp system
-	if (file_type.find("py") != std::string::npos)
-		return true;
-	return false;
-}
-
-void Response::pipeCreatorAndExec(char **argv) {
-    if (pipe(this->_pipe_out) < 0) {
-        std::cout << "pipe1 marche po" << std::endl;
-        exit(1);
-    }
-    if (pipe(this->_pipe_in) < 0) {
-        std::cout << "pipe2 marche po" << std::endl;
-        exit(1);
-    }
-    _pid = fork();
-    if (_pid < 0) {
-        std::cout << "fork marche po" << std::endl;
-        exit(1);
-    }
-
-    if (_pid == 0) {
-        dup2(_pipe_in[0], STDIN_FILENO);
-        dup2(_pipe_out[1], STDOUT_FILENO);
-        closeAllPipe();
-        exit(execve(argv[0], argv, this->_env));
-    }
-}
-
-void Response::closeAllPipe() {
-    close(_pipe_in[0]);
-    close(_pipe_in[1]);
-    close(_pipe_out[0]);
-    close(_pipe_out[1]);
-}
-
-std::string intToString(int num) {
-    std::ostringstream oss;
-    oss << num;
-    return oss.str();
 }
 
 std::string Response::getCodeHeader(std::string * path, Server* server,  const std::string & uri) {
@@ -350,6 +243,10 @@ void Response::handleRequestError(Server* server, const std::string & uri) {
 
 std::string & Response::getContent() {
     return this->_content;
+}
+
+void Response::setContent(const std::string &content) {
+    this->_content = content;
 }
 
 std::string &Response::getUri() {
