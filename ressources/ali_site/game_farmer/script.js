4951561d9860172892ec54408bf5346230f3476d
AliHaine <alitr57120@gmail.com>
2024-03-23 18:55:50 +0100

    game update

--- ressources/ali_site/game_farmer/script.js
+++ ressources/ali_site/game_farmer/script.js
@@ -1,34 +1,34 @@
 // Calculate the number of squares horizontally and vertically based on the screen size
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-const squaresPerRow = Math.floor(screenWidth / 64) - 2;
-const numRows = Math.floor(screenHeight / 64);
+map = document.getElementById('map');
+
+result = getPercent(screen.width, 95);
+map.style.width = result - (result % 64) + "px";
+result = getPercent(screen.height, 80);
+map.style.height = result - (result % 64) + "px";
+
+const squaresPerRow = Math.floor(map.clientWidth / 64);
+const numRows = Math.floor(map.clientHeight / 64);
+
 
 let isDrawing = false;
 let isErasing = false;
 
 const character = new Character(500, 500, document.getElementById('characterimg'));
 
-document.addEventListener('keydown', moveListener);
 
-// Create grid dynamically
+document.addEventListener('keydown', moveListener);
+map.addEventListener('mousedown', mouseDownEvent);
+map.addEventListener('mousemove', mouseMoveEvent);
+map.addEventListener('mouseup', stopDrawing);
+
 const container = document.body;
 for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < squaresPerRow; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.style.backgroundColor = 'lightgray';
-        square.addEventListener('mousedown', mouseDownEvent);
-        square.addEventListener('mousemove', mouseMoveEvent);
-        square.addEventListener('mouseup', stopDrawing);
-        container.appendChild(square);
+        map.appendChild(square);
     }
-    const clearfix = document.createElement('div');
-    clearfix.classList.add('clearfix');
-    container.appendChild(clearfix);
 }
-
-
 
 function erase(square) {
     square.style.backgroundColor = 'lightgray';
@@ -41,11 +41,9 @@ function draw(square) {
 
 function drawOnSquare() {
     // Calculate the index of the square in the linear grid
-    let row = Math.floor(character.positionY / 64)
-    let column = Math.floor(character.positionX / 64)
+    let row = Math.floor((character.positionY - 45) / 64);
+    let column = Math.floor((character.positionX - 70) / 64);
     const index = row * squaresPerRow + column;
-    //draw(document.querySelectorAll('.square')[index])
-    console.log(index);
     document.querySelectorAll('.square')[index].appendChild(loadImage("../image/4.png"))
 }
 
--- ressources/ali_site/game_farmer/utils.js
+++ ressources/ali_site/game_farmer/utils.js
@@ -1,8 +1,12 @@
-    function loadImage(imagePath, square) {
+function loadImage(imagePath) {
     const img = new Image();
     img.src = imagePath;
     img.onerror = function() {
         console.error(`Failed to load image into square`);
     };
     return img;
+}
+
+function getPercent(value, percent) {
+    return (value * percent) / 100;
 }
